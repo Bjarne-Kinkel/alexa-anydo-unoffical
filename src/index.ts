5a238dd77eedb836fc742b0737f7095023775909
Bjarne <2.bjarne.kinkel@gmail.com>
2023-01-04 20:17:24 +0100

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from 'express';
-import Alexa from 'ask-sdk';
-import { ExpressAdapter } from 'ask-sdk-express-adapter';
+// import Alexa from 'ask-sdk';
+// import { ExpressAdapter } from 'ask-sdk-express-adapter';
 
 
 const app = express();
 
-const skill = Alexa.SkillBuilders.custom().addRequestHandlers().create();
-const adapter = new ExpressAdapter(skill, true, true);
+// const skill = Alexa.SkillBuilders.custom().addRequestHandlers().create();
+// const adapter = new ExpressAdapter(skill, true, true);
 
-app.post('/skill', adapter.getRequestHandlers());
+// app.post('/skill', adapter.getRequestHandlers());
 
 app.all('/skill', (req: Request, res: Response) => {
     console.log(req);
