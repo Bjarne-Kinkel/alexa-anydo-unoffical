6cd54439caab0b93399abc8977c5cbc21e5d67d7
Bjarne <2.bjarne.kinkel@gmail.com>
2023-01-04 20:03:53 +0100

    Added foundation for skill

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express, { Request, Response } from 'express';
+import Alexa from 'ask-sdk';
+import { ExpressAdapter } from 'ask-sdk-express-adapter';
+
+
 const app = express();
 
-app.all('/', (req: Request, res: Response) => {
+const skill = Alexa.SkillBuilders.custom().addRequestHandlers().create();
+const adapter = new ExpressAdapter(skill, true, true);
+
+app.post('/skill', adapter.getRequestHandlers());
+
+app.all('/skill', (req: Request, res: Response) => {
     console.log(req);
     res.send('Yo!');
 });
